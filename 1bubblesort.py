b8c124aaf40c100b09a0f0900c33ab8457f129bc
quantan <quantan@ht.sfc.keio.ac.jp>
2017-08-31 14:06:49 +0900

    bubble

--- 1bubblesort.py
+++ 1bubblesort.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
+import random
 
 
-def buublesort(array):
+def bubblesort(array):
+    count = 0
     for i in range(len(array) - 1):
+        array2 = array[:]
+        print(count)
+        count += 1
         for j in range(len(array) - 1):
             print(array)
             if array[j] < array[j + 1]:
                 temp = array[j]
                 array[j] = array[j + 1]
                 array[j + 1] = temp
+        if array2 == array:
+            break
 
 
 def main():
-    a = [88, 11, 55, 22, 100, 33, 99, 77, 44, 66]
-    print(buublesort(a))
+    a = list(range(10))
+    random.shuffle(a)
+    print(bubblesort(a))
 
 
 if __name__ == '__main__':
